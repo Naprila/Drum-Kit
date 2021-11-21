6b99c163eaca9a46f5353fd65349bd6b48a9b222
Naprila <85901005+Naprila@users.noreply.github.com>
2021-11-21 15:45:04 +0530

    Initial files added.

---
+++ index.js
@@ -0,0 +1,68 @@
+var noButtons = document.querySelectorAll(".drum");
+
+for(var i=0;i<noButtons.length;i++)
+{
+    noButtons[i].addEventListener("click", function(){
+
+      var key = this.innerHTML;
+      makeSound(key);
+      makeAnimation(key);
+    });
+
+    document.addEventListener("keypress",function(event){
+      makeSound(event.key);
+      makeAnimation(event.key);
+    });
+}
+
+function makeSound(key)
+{
+      switch (key) {
+        case "w":
+                  var audio = new Audio("sounds/kick-bass.mp3");
+                  audio.play();
+                  break;
+        case "a":
+                  var audio = new Audio("sounds/snare.mp3");
+                  audio.play();
+                  break;
+        case "s":
+                  var audio = new Audio("sounds/crash.mp3");
+                  audio.play();
+                  break;
+        case "d":
+                  var audio = new Audio("sounds/tom-1.mp3");
+                  audio.play();
+                  break;
+        case "j":
+                  var audio = new Audio("sounds/tom-2.mp3");
+                  audio.play();
+                  break;
+        case "k":
+                  var audio = new Audio("sounds/tom-3.mp3");
+                  audio.play();
+                  break;
+        case "l":
+                  var audio = new Audio("sounds/tom-4.mp3");
+                  audio.play();
+                  break;
+
+
+        default:console.log(event.key +" is pressed!");
+              }
+}
+
+
+function makeAnimation(key)
+{
+  var pressedButton = document.querySelector("."+key);
+  pressedButton.classList.add("pressed");
+  pressedButton.classList.add("white");
+
+
+  setTimeout(function(){
+  pressedButton.classList.remove("pressed");
+  pressedButton.classList.remove("white");
+},100);
+
+}
